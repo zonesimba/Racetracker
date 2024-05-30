65eedf2bae87a2ca8fb66467af0c5d6d416c88c9
kakudji simba gentil <21ks128@esisalama.org>
2024-05-30 13:00:11 +0200

    modification de RaceParticipant

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -20,9 +20,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.delay
 
-/**
- * This class represents a state holder for race participant.
- */
 class RaceParticipant(
     val name: String,
     val maxProgress: Int = 100,
@@ -35,16 +32,9 @@ class RaceParticipant(
         require(progressIncrement > 0) { "progressIncrement=$progressIncrement; must be > 0" }
     }
 
-    /**
-     * Indicates the race participant's current progress
-     */
     var currentProgress by mutableStateOf(initialProgress)
         private set
 
-    /**
-     * Updates the value of [currentProgress] by value [progressIncrement] until it reaches
-     * [maxProgress]. There is a delay of [progressDelayMillis] between each update.
-     */
     suspend fun run() {
         while (currentProgress < maxProgress) {
             delay(progressDelayMillis)
@@ -52,18 +42,10 @@ class RaceParticipant(
         }
     }
 
-    /**
-     * Regardless of the value of [initialProgress] the reset function will reset the
-     * [currentProgress] to 0
-     */
     fun reset() {
         currentProgress = 0
     }
 }
 
-/**
- * The Linear progress indicator expects progress value in the range of 0-1. This property
- * calculate the progress factor to satisfy the indicator requirements.
- */
 val RaceParticipant.progressFactor: Float
     get() = currentProgress / maxProgress.toFloat()
